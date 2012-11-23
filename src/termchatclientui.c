e524e10861534aa0303ef1319da9d592790a5c51
endrepalinkas <palinkas@endre.me>
2012-11-23 11:31:45 +0100

    nicklist, chat, and input windows

--- src/termchatclientui.c
+++ src/termchatclientui.c
@@ -6,9 +6,15 @@
 WINDOW *create_newwin(int height, int width, int starty, int startx);
 void destroy_win(WINDOW *local_win);
 
-int main(int argc, char *argv[])
-{	WINDOW *main_win;
-	int main_win_startx, main_win_starty, main_win_width, main_win_height;
+int main(int argc, char *argv[]) {
+	WINDOW *main_win;
+	WINDOW *nicklist_win;
+	WINDOW *input_win;
+	WINDOW *chat_win;
+	//int main_win_startx, main_win_starty, main_win_width, main_win_height;
+	int nicklist_win_startx, nicklist_win_starty, nicklist_win_width, nicklist_win_height;
+	int input_win_startx, input_win_starty, input_win_width, input_win_height;
+	int chat_win_startx, chat_win_starty, chat_win_width, chat_win_height;
 	int ch;
 	// start curses mode
 	initscr();
@@ -16,32 +22,43 @@ int main(int argc, char *argv[])
 	cbreak();
 	keypad(stdscr, TRUE);
 
-	// set size for main window
-	main_win_height = LINES;
-	main_win_width = COLS;
-	main_win_starty = 0;
-	main_win_startx = 0;
+	// set size for windows
+	nicklist_win_height = LINES-3;
+	nicklist_win_width = 14;
+	nicklist_win_starty = 0;
+	nicklist_win_startx = COLS-14;
+	input_win_height = 3;
+	input_win_width = COLS;
+	input_win_starty = LINES-3;
+	input_win_startx = 0;
+	chat_win_height = LINES-3;
+	chat_win_width = COLS-14;
+	chat_win_starty = 0;
+	chat_win_startx = 0;	
 
 	refresh();
-	main_win = create_newwin(main_win_height, main_win_width, main_win_starty, main_win_startx);
+	//main_win = create_newwin(main_win_height, main_win_width, main_win_starty, main_win_startx);
+	nicklist_win = create_newwin(nicklist_win_height, nicklist_win_width, nicklist_win_starty, nicklist_win_startx);
+	input_win = create_newwin(input_win_height, input_win_width, input_win_starty, input_win_startx);
+	chat_win = create_newwin(chat_win_height, chat_win_width, chat_win_starty, chat_win_startx);
 
 	while((ch = getch()) != KEY_F(10))
 	{	switch(ch)
 		{	case KEY_LEFT:
-				destroy_win(main_win);
-				main_win = create_newwin(main_win_height, main_win_width, main_win_starty,--main_win_startx);
+				//destroy_win(main_win);
+				//main_win = create_newwin(main_win_height, main_win_width, main_win_starty,--main_win_startx);
 				break;
 			case KEY_RIGHT:
-				destroy_win(main_win);
-				main_win = create_newwin(main_win_height, main_win_width, main_win_starty,++main_win_startx);
+				//destroy_win(main_win);
+				//main_win = create_newwin(main_win_height, main_win_width, main_win_starty,++main_win_startx);
 				break;
 			case KEY_UP:
-				destroy_win(main_win);
-				main_win = create_newwin(main_win_height, main_win_width, --main_win_starty,main_win_startx);
+				//destroy_win(main_win);
+				//main_win = create_newwin(main_win_height, main_win_width, --main_win_starty,main_win_startx);
 				break;
 			case KEY_DOWN:
-				destroy_win(main_win);
-				main_win = create_newwin(main_win_height, main_win_width, ++main_win_starty,main_win_startx);
+				//destroy_win(main_win);
+				//main_win = create_newwin(main_win_height, main_win_width, ++main_win_starty,main_win_startx);
 				break;	
 		}
 	}
