e6d9203e9bdc23a85a77887ef81cc844f402eeaf
endrepalinkas <palinkas@endre.me>
2012-12-03 21:50:50 +0100

    don't allow too long nicks

--- src/termchatserver.c
+++ src/termchatserver.c
@@ -411,6 +411,12 @@ void ProcessClientChangeNick(int clientindex, const char *cmd_msg) {
 			return;
 		}
 		
+		if (strlen(newnick)>MAX_NICK_LENGTH -1) {
+				sprintf(reply, "CHANGENICKERROR Nick is too long.\n");
+				send(chat_clients[clientindex].socket, reply, strlen(reply), 0);
+				return;
+		}
+		
 		// if client already set a nick before, let's check if this nick is different
 		if (chat_clients[clientindex].status != WAITING_FOR_NICK ) {
 			if (!strcmp(newnick, chat_clients[clientindex].nickname)) {
